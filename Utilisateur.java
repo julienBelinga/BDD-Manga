a9853072f15f192b125508a41b9e4b8ea4459e5e
julienBelinga <julienbelinga29@gmail.com>
2020-02-13 17:25:18 +0100

    started to take each line for a variable of manga
    ain't finish

--- Utilisateur.java
+++ Utilisateur.java
@@ -7,17 +7,28 @@ import java.io.IOException;
 public class Utilisateur {
 
     private Manga manga;
-    
-    public void comparer() throws IOException {
+    private String tabReader[];
+
+    public void lire() throws IOException {
         String file;
-        try ( BufferedReader br = new BufferedReader(new FileReader("liste.txt"))) {
+        try (BufferedReader br = new BufferedReader(new FileReader("liste.txt"))) {
             String line;
             while ((line = br.readLine()) != null) {
-                // process the line.
-                System.out.println(line);
 
+                System.out.println(line);
+                for (int i = 0; i < 7; i++) {
+                    tabReader[i] = line;
+                }
+                
+                int annee = Integer.parseInt(tabReader[4]);
+                int nbTome = Integer.parseInt(tabReader[5]);
+                
+                tabReader[0] = manga.getTitre();
+                tabReader[1] = manga.getEditeur();
+                tabReader[2] = manga.getGenre();
+                tabReader[3] = manga.getAuteur();
+                                            
             }
         }
     }
 }
-      
